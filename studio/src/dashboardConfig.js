175c9be135c30ae04427bff16d42d24a715b1d32
Jackie Luc <jackieluc17@gmail.com>
2020-03-02 00:03:35 -0700

    refactor(sanity-dashboard): update layout and integrations

--- studio/src/dashboardConfig.js
+++ studio/src/dashboardConfig.js
@@ -11,12 +11,12 @@ export default {
               description:
                 'NOTE: Because these sites are static builds, they need to be re-deployed to see the changes when documents are published.',
               sites: [
-                {
-                  buildHookId: '5e5c4e710f8528e07c45448a',
-                  title: 'Sanity Studio',
-                  name: 'jackieluc-website-studio',
-                  apiId: 'e53ef859-e41e-491e-a8dd-9c6f92c75d7f'
-                },
+                // {
+                //   buildHookId: '5e5c4e710f8528e07c45448a',
+                //   title: 'Sanity Studio',
+                //   name: 'jackieluc-website-studio',
+                //   apiId: 'e53ef859-e41e-491e-a8dd-9c6f92c75d7f'
+                // },
                 {
                   buildHookId: '5e5c4e710f8528c1194547a9',
                   title: 'Blog Website',
@@ -29,19 +29,37 @@ export default {
         ],
         data: [
           {
-            title: 'GitHub repo',
+            title: 'GitHub Repo',
             value: 'https://github.com/jackieluc/jackieluc-website',
             category: 'Code'
           },
-          { title: 'Frontend', value: 'https://jackieluc-website.netlify.com', category: 'apps' }
+          {
+            title: 'Website',
+            value: 'https://jackieluc-website.netlify.com',
+            category: 'apps'
+          }
         ]
+      },
+      layout: {
+        width: 'large',
+        height: 'large'
       }
     },
-    { name: 'project-users', layout: { height: 'auto' } },
     {
       name: 'document-list',
-      options: { title: 'Recent blog posts', order: '_createdAt desc', types: ['post'] },
-      layout: { width: 'medium' }
+      options: {
+        title: 'Recent blog posts',
+        order: '_createdAt desc',
+        types: ['post']
+      },
+      layout: {
+        width: 'medium',
+        height: 'large'
+      }
+    },
+    {
+      name: 'project-users',
+      layout: { height: 'auto' }
     }
   ]
 }
