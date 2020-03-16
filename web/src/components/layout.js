ec46e64ab651d394c8f04afba53b0fdfb1cd85dc
Jackie Luc <jackieluc17@gmail.com>
2020-03-15 23:05:20 -0600

    nav - slight cleanup/code removal

--- web/src/components/layout.js
+++ web/src/components/layout.js
@@ -8,9 +8,6 @@ const Layout = ({ children, onHideNav, onShowNav, showNav, siteTitle }) => (
   <div>
     <Header
       siteTitle={siteTitle}
-      onHideNav={onHideNav}
-      onShowNav={onShowNav}
-      showNav={showNav}
     />
     <main className={styles.content}>
       {children}
