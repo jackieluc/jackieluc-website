e33bc793252d8cb85cc6b09855f39cb86b03fed3
Jackie Luc <jackieluc17@gmail.com>
2020-03-15 23:05:41 -0600

    nav - remove unneeded code

--- web/src/components/layout.js
+++ web/src/components/layout.js
@@ -4,7 +4,7 @@ import Header from './header'
 import '../styles/layout.css'
 import styles from './layout.module.css'
 
-const Layout = ({ children, onHideNav, onShowNav, showNav, siteTitle }) => (
+const Layout = ({ children, siteTitle }) => (
   <div>
     <Header
       siteTitle={siteTitle}
