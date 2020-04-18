ec35d18a5d702bd54a259e17ba670fa9f421c126
Jackie Luc <jackieluc17@gmail.com>
2020-04-18 00:22:11 -0600

    feat(fonts): add Fira Sans and Lora

--- web/gatsby-config.js
+++ web/gatsby-config.js
@@ -58,6 +58,14 @@ module.exports = {
         }
       }
     },
-    'gatsby-plugin-styled-components'
+    'gatsby-plugin-styled-components',
+    {
+      resolve: 'gatsby-plugin-web-font-loader',
+      options: {
+        google: {
+          families: ['Fira Sans', 'Lora']
+        }
+      }
+    }
   ]
 }
