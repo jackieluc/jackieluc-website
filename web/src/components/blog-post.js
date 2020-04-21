fa4eef87d965a1979cf63fa8672e4ee625bd9fcd
Jackie Luc <jackieluc17@gmail.com>
2020-04-20 22:57:35 -0600

    chore: blog-post add main-image

--- web/src/components/blog-post.js
+++ web/src/components/blog-post.js
@@ -1,8 +1,7 @@
 import { format, distanceInWords, differenceInDays } from 'date-fns'
 import React from 'react'
 import { buildImageObj, getReadingTime } from '../lib/helpers'
-// import { imageUrlFor } from '../lib/image-url'
-// import PortableText from './portableText'
+import { imageUrlFor } from '../lib/image-url'
 import ReactMarkdown from 'react-markdown'
 import Container from './container'
 import Heading from './markdown-renderers/Heading'
@@ -18,19 +17,6 @@ function BlogPost (props) {
   const { body, tags, title, mainImage, publishedAt } = props
   return (
     <article className={styles.root}>
-      {/* {mainImage && mainImage.asset && (
-        <div className={styles.mainImage}>
-          <img
-            src={imageUrlFor(buildImageObj(mainImage))
-              .width(1200)
-              .height(Math.floor((9 / 16) * 1200))
-              .fit('crop')
-              .auto('format')
-              .url()}
-            alt={mainImage.alt}
-          />
-        </div>
-      )} */}
       <Container>
         <div className={styles.grid}>
           <div className={styles.mainContent}>
@@ -60,6 +46,19 @@ function BlogPost (props) {
                 </div>
               )}
             </div>
+            {mainImage && mainImage.asset && (
+              <div className={styles.mainImage}>
+                <img
+                  src={imageUrlFor(buildImageObj(mainImage))
+                    .width(1200)
+                    .height(Math.floor((9 / 16) * 1200))
+                    .fit('crop')
+                    .auto('format')
+                    .url()}
+                  alt={mainImage.alt}
+                />
+              </div>
+            )}
             {body &&
               <ReactMarkdown
                 source={body}
