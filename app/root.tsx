4f4ad312c5c06e515e0e6e1d13562842a9adcb87
Jackie Luc <15662837+jackieluc@users.noreply.github.com>
2022-06-12 02:03:05 -0700

    feat: add initial SEO

--- app/root.tsx
+++ app/root.tsx
@@ -1,23 +1,47 @@
 import type { MetaFunction } from '@remix-run/node';
 import type { LinksFunction } from '@remix-run/node';
-import {
-  Links,
-  LiveReload,
-  Meta,
-  Outlet,
-  Scripts,
-  ScrollRestoration,
-} from '@remix-run/react';
+import { Links, LiveReload, Meta, Outlet, Scripts, ScrollRestoration } from '@remix-run/react';
 
+import favicon from '../public/favicon.png';
 import styles from './tailwind.css';
 
-export const links: LinksFunction = () => [{ rel: 'stylesheet', href: styles }];
+export const links: LinksFunction = () => [
+  { rel: 'icon', href: favicon },
+  { rel: 'stylesheet', href: styles },
+];
 
-export const meta: MetaFunction = () => ({
-  charset: 'utf-8',
-  title: 'New Remix App',
-  viewport: 'width=device-width,initial-scale=1',
-});
+// !TODO: add SEO images
+export const meta: MetaFunction = () => {
+  const name = 'Jackie Luc';
+  const description = 'Trying to learn, grow, and build solutions that magnify the impact of others.';
+  const url = 'https://www.jackieluc.com';
+
+  return {
+    language: 'English',
+    charset: 'utf-8',
+    title: name,
+    viewport: 'width=device-width,initial-scale=1',
+    description,
+    keywords: 'jackie, jackie luc, blog, learning, software, career, professional, growth, inspiration',
+    robots: 'index, follow',
+    author: name,
+    'og:title': name,
+    'og:type': 'website',
+    'og:image': '',
+    'og:image:alt': '',
+    'og:url': url,
+    'og:description': description,
+    'og:site_name': name,
+    'twitter:card': 'summary_large_image',
+    'twitter:domain': url,
+    'twitter:url': url,
+    'twitter:title': name,
+    'twitter:description': description,
+    'twitter:image': '',
+    'twitter:image:alt': '',
+    'twitter:site': '@jackiesthinking',
+  };
+};
 
 export default function App() {
   return (
