558ad16ad37323c3063ca74cd9cc8a5032edc844
Jackie Luc <15662837+jackieluc@users.noreply.github.com>
2022-07-21 21:11:48 -0700

    remove remix

--- app/components/markdoc/tags/callout.tsx
+++
@@ -1,3 +0,0 @@
-export default function Callout({ children }: any) {
-  return <div className='callout bg-red-400 text-2xl'>{children}</div>;
-}
--- app/utils/utils.test.ts
+++
@@ -1,8 +0,0 @@
-import { add } from './utils';
-
-// The two tests marked with concurrent will be run in parallel
-describe('utils', () => {
-  it('add two numbers', () => {
-    expect(add(1, 2)).toEqual(3);
-  });
-});
