9218a5ab73a7ab05879765acf698b003072a076b
Jackie Luc <15662837+jackieluc@users.noreply.github.com>
2022-07-21 22:13:45 -0700

    add nextjs, ts, tailwind, vitest

---
+++ __tests__/utils.test.ts
@@ -0,0 +1,9 @@
+import { describe, it, expect } from 'vitest';
+import { add } from '@utils/addTwoNumbers';
+
+// The two tests marked with concurrent will be run in parallel
+describe('utils', () => {
+  it('add two numbers', () => {
+    expect(add(1, 2)).toEqual(3);
+  });
+});
