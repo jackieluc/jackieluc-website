5e3bc11d29f4850307b76edb3534cf206f90ddc2
Jackie Luc <15662837+jackieluc@users.noreply.github.com>
2022-07-24 02:55:40 -0700

    update vitest version to latest

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,7 @@
 import type { NextPage } from 'next';
 import Head from 'next/head';
 import Image from 'next/image';
-import Button from 'src/components/button';
+import Button from '@/components/button';
 
 const Home: NextPage = () => {
   return (
