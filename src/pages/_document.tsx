4d59007e91b581a6adef750e8fc43f96f35eb5ae
Jackie Luc <15662837+jackieluc@users.noreply.github.com>
2022-08-20 20:36:57 -0700

    chore(layout): move layout down for large breakpoint to start after nav

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -4,7 +4,7 @@ export default function Document() {
   return (
     <Html lang='en'>
       <Head />
-      <body className='mx-auto max-w-5xl'>
+      <body className='mx-auto mt-16 max-w-5xl'>
         <Main />
         <NextScript />
       </body>
