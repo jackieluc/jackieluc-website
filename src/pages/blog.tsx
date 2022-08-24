bf5ac56cc9f614bc45c484cf5c56d6d8c7ca0c5e
Jackie Luc <15662837+jackieluc@users.noreply.github.com>
2022-08-23 19:25:55 -0700

    feat(blog): styling changes

--- src/pages/blog.tsx
+++ src/pages/blog.tsx
@@ -12,54 +12,42 @@ export default function Blog({
   }[];
 }) {
   return (
-    <main className='grid h-screen place-items-center'>
-      <h1>Hello blog</h1>
-      <Link href='/'>what</Link>
-      <div>
-        <ul>
-          {allBlogPostProperties.map(({ properties }: { properties: BlogProperties }) => {
-            return (
-              <li className='cursor-pointer gap-2 border-4 border-cyan-200 p-4' key={getSlug(properties.title)}>
-                <Link href={`/blog/${getSlug(properties.title)}`}>
-                  <div className='flex flex-col gap-y-2'>
-                    <h2 className='text-xl font-bold'>{properties.title}</h2>
-                    <h3 className='text-md text-primary font-normal'>{properties.subtitle}</h3>
-                    {properties.category ? (
-                      <Link href={`/category/${getSlug(properties.category)}`}>
-                        <a className='max-w-fit rounded-full bg-red-200 px-4 py-2 text-sm font-medium text-gray-800 no-underline hover:bg-gray-300'>
-                          {properties.category}
-                        </a>
-                      </Link>
-                    ) : null}
-                    {properties.tags.length > 0 ? (
-                      <ul className='flex'>
-                        {properties.tags.map((tag: NotionTag) => (
-                          <li key={getSlug(tag.name)}>
-                            <Link href={`/tags/${getSlug(tag.name)}`}>
-                              <a className='max-w-fit rounded-full bg-gray-100 px-4 py-2 text-xs font-medium text-gray-800 no-underline hover:bg-gray-300'>
-                              {tag.name}
-                              </a>
-                          </Link>
-                          </li>
-                        ))}
-                      </ul>
-                    ) : null}
-                    <div>
-                      <p>Views: {properties.views}</p>
-                    </div>
-                    <div>
-                      <p>Upvotes: {properties.upvotes}</p>
-                    </div>
-                    <div>
-                      <p>Published: {properties.published}</p>
-                    </div>
+    <main className='mt-16'>
+      <ul className='mx-auto grid max-w-prose gap-4'>
+        {allBlogPostProperties.map(({ properties }: { properties: BlogProperties }) => {
+          return (
+            <li className='border-secondary rounded-md border-2 p-8' key={getSlug(properties.title)}>
+              <Link href={`/blog/${getSlug(properties.title)}`}>
+                <a className='flex flex-col gap-y-2 no-underline'>
+                  <h2 className='text-xl font-bold'>{properties.title}</h2>
+                  <h3 className='text-md'>{properties.subtitle}</h3>
+                  {properties.tags.length > 0 ? (
+                    <ul className='flex'>
+                      {properties.tags.map((tag: NotionTag) => (
+                        <li
+                          className='max-w-fit rounded-full bg-gray-100 px-4 py-2 text-xs font-medium text-gray-800'
+                          key={getSlug(tag.name)}
+                        >
+                          {tag.name}
+                        </li>
+                      ))}
+                    </ul>
+                  ) : null}
+                  <div>
+                    <p>Views: {properties.views}</p>
                   </div>
-                </Link>
-              </li>
-            );
-          })}
-        </ul>
-      </div>
+                  <div>
+                    <p>Upvotes: {properties.upvotes}</p>
+                  </div>
+                  <div>
+                    <p>Published: {properties.published}</p>
+                  </div>
+                </a>
+              </Link>
+            </li>
+          );
+        })}
+      </ul>
     </main>
   );
 }
@@ -73,5 +61,6 @@ export async function getStaticProps() {
     props: {
       allBlogPostProperties,
     },
+    // TODO: add revalidate for every day
   };
 }
