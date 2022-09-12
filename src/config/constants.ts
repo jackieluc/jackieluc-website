a94a65f48f71b1f67f3d9ea67f047efd57b41758
Jackie Luc <15662837+jackieluc@users.noreply.github.com>
2022-09-12 01:14:04 -0700

    fix(seo): og:url use full image path

--- src/config/constants.ts
+++ src/config/constants.ts
@@ -5,7 +5,7 @@ export const SITE_DESCRIPTION =
   'Embark on a life-long journey to grow your career in software development and create great web experiences with me.';
 export const SITE_KEYWORDS =
   'learn, career, software, dev, developer, engineer, development, software engineer, web, javascript, react, nextjs, kubernetes, blog, self improvement, coding, programming';
-export const SITE_IMAGE = '/default-seo-image.jpg';
+export const SITE_IMAGE = `${SITE_URL}/default-seo-image.jpg`;
 export const SITE_IMAGE_ALT =
   "A mesh gradient background with two lines of text for jackieluc.com and the site's description.";
 export const SITE_PAGES = [
