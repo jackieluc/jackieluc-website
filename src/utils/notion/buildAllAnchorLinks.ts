4be9c0a85b9836a7dc301f595e1fc3f5c56f6624
Jackie Luc <15662837+jackieluc@users.noreply.github.com>
2022-10-12 01:41:18 -0700

    refactor: use textItem var

--- src/utils/notion/buildAllAnchorLinks.ts
+++ src/utils/notion/buildAllAnchorLinks.ts
@@ -17,12 +17,12 @@ export default async function buildAllAnchorLinks(content: BlockObjectResponse[]
       }
 
       for (let i = 0; i < block.paragraph.rich_text.length; i++) {
-        const textItem = block.paragraph.rich_text[i];
+        let textItem = block.paragraph.rich_text[i];
 
         if (textItem?.href?.startsWith('/') && textItem.href.includes('#')) {
           const blockId = textItem.href.split('#')[1];
           const anchorBlock = (await getBlock(blockId)) as any;
-          block.paragraph.rich_text[i].href = `#${getSlug(anchorBlock[anchorBlock.type]?.rich_text[0].plain_text)}`;
+          textItem.href = `#${getSlug(anchorBlock[anchorBlock.type]?.rich_text[0].plain_text)}`;
         }
       }
 
